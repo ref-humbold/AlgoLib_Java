05c908890406196c2b6723f68dbdf00b5999c2b4
R. Kaleta <rafkaleta@onet.pl>
2019-05-26 00:29:26 +0200

    Created Subseqs class

---
+++ src/refhumbold/algolib/Subseqs.java
@@ -0,0 +1,161 @@
+// Algorithms for subsequences.
+package refhumbold.algolib;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+import refhumbold.algolib.tuples.ImmutablePair;
+
+public class Subseqs
+{
+    /**
+     * Constructs longest ordered subsequence.
+     * @param sequence sequence of elements
+     * @param order order function of elements in subsequence
+     * @return least lexicographically longest ordered subsequence
+     */
+    public static <T> Collection<T> longestOrdered(List<T> sequence, BiPredicate<T, T> order)
+    {
+        List<Integer> previousElem = new ArrayList<>(Collections.nCopies(sequence.size(), null));
+        List<Integer> subseqLast = new ArrayList<>();
+
+        subseqLast.add(0);
+
+        for(int i = 1; i < sequence.size(); ++i)
+        {
+            T elem = sequence.get(i);
+
+            if(order.test(elem, sequence.get(subseqLast.get(subseqLast.size() - 1))))
+            {
+                previousElem.set(i, subseqLast.get(subseqLast.size() - 1));
+                subseqLast.add(i);
+            }
+            else
+            {
+                int index = searchOrd(order, sequence, subseqLast, 0, subseqLast.size() - 1, i);
+
+                subseqLast.set(index, i);
+                previousElem.set(i, index > 0 ? subseqLast.get(index - 1) : null);
+            }
+        }
+
+        List<T> longestSubseq = new ArrayList<>();
+        Integer j = subseqLast.get(subseqLast.size() - 1);
+
+        while(j != null)
+        {
+            longestSubseq.add(sequence.get(j));
+            j = previousElem.get(j);
+        }
+
+        Collections.reverse(longestSubseq);
+
+        return longestSubseq;
+    }
+
+    /**
+     * Dynamically constructs coherent subarray with maximal sum.
+     * @param sequence sequence of numbers
+     * @return maximum subarray
+     */
+    public static List<Double> maximumSubarray(Iterable<Double> sequence)
+    {
+        ImmutablePair<Double, List<Double>> actual = ImmutablePair.make(0.0, new ArrayList<>());
+        ImmutablePair<Double, List<Double>> maximal = ImmutablePair.make(0.0, new ArrayList<>());
+
+        for(Double elem : sequence)
+        {
+            if(actual.getFirst() < 0.0)
+                actual = ImmutablePair.make(0.0, new ArrayList<>());
+
+            actual = ImmutablePair.make(actual.getFirst() + elem, actual.getSecond());
+            actual.getSecond().add(elem);
+
+            if(actual.getFirst() > maximal.getFirst())
+                maximal =
+                        ImmutablePair.make(actual.getFirst(), new ArrayList<>(actual.getSecond()));
+        }
+
+        return maximal.getSecond();
+    }
+
+    /**
+     * Counts maximal sum from all coherent subarrays using interval tree.
+     * @param sequence sequence of numbers
+     * @return the sum of maximum subarray
+     */
+    public static double maximalSubsum(Collection<Double> sequence)
+    {
+        int size = 1;
+
+        while(size < 2 * sequence.size())
+            size <<= 1;
+
+        List<Double> intervalSums = new ArrayList<>(Collections.nCopies(size, 0.0));
+        List<Double> prefixSums = new ArrayList<>(Collections.nCopies(size, 0.0));
+        List<Double> suffixSums = new ArrayList<>(Collections.nCopies(size, 0.0));
+        List<Double> allSums = new ArrayList<>(Collections.nCopies(size, 0.0));
+
+        int i = 0;
+
+        for(Double elem : sequence)
+        {
+            int index = size / 2 + i;
+
+            allSums.set(index, allSums.get(index) + elem);
+            intervalSums.set(index, Math.max(allSums.get(index), 0.0));
+            prefixSums.set(index, Math.max(allSums.get(index), 0.0));
+            suffixSums.set(index, Math.max(allSums.get(index), 0.0));
+            index /= 2;
+            ++i;
+
+            while(index > 0)
+            {
+                int indexLeft = index + index;
+                int indexRight = index + index + 1;
+
+                intervalSums.set(index, Math.max(
+                        Math.max(intervalSums.get(indexRight), intervalSums.get(indexLeft)),
+                        suffixSums.get(indexRight) + prefixSums.get(indexLeft)));
+                prefixSums.set(index, Math.max(prefixSums.get(indexRight),
+                                               allSums.get(indexRight) + prefixSums.get(
+                                                       indexLeft)));
+                suffixSums.set(index, Math.max(suffixSums.get(indexLeft),
+                                               suffixSums.get(indexRight) + allSums.get(
+                                                       indexLeft)));
+                allSums.set(index, allSums.get(indexRight) + allSums.get(indexLeft));
+                index /= 2;
+            }
+        }
+
+        return intervalSums.get(1);
+    }
+
+    /**
+     * Searches for place of element in list of subsequences.
+     * @param order order function of elements in subsequence
+     * @param sequence input sequence
+     * @param subseqLast last elements of subsequences
+     * @param indexBegin index of beginning
+     * @param indexEnd index of end
+     * @param indexElem index of current element
+     * @return index in list of subsequences
+     */
+    private static <T> int searchOrd(BiPredicate<T, T> order, List<T> sequence,
+                                     List<Integer> subseqLast, int indexBegin, int indexEnd,
+                                     int indexElem)
+    {
+        if(indexBegin == indexEnd)
+            return indexBegin;
+
+        int indexMiddle = (indexBegin + indexEnd) / 2;
+
+        if(order.test(sequence.get(indexElem), sequence.get(subseqLast.get(indexMiddle))))
+            return searchOrd(order, sequence, subseqLast, indexMiddle + 1, indexEnd, indexElem);
+        else
+            return searchOrd(order, sequence, subseqLast, indexBegin, indexMiddle, indexElem);
+    }
+}
--- test/refhumbold/algolib/SubseqsTest.java
+++ test/refhumbold/algolib/SubseqsTest.java
@@ -1,4 +1,4 @@
-// TESTY DLA PODCIĄGU SPÓJNEGO O MAKSYMALNEJ SUMIE
+// TEST : Algorithms for subsequences.
 package refhumbold.algolib;
 
 import java.util.Arrays;
@@ -8,79 +8,77 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MaximumSubarrayTest
+public class SubseqsTest
 {
     private List<Double> sequence = null;
 
     @Before
     public void setUp()
-        throws Exception
     {
     }
 
     @After
     public void tearDown()
-        throws Exception
     {
         sequence = null;
     }
 
     @Test
-    public void testFindMaximumSubarray1()
+    public void testMaximumSubarray1()
     {
         sequence = Arrays.asList(3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8);
 
-        List<Double> result = MaximumSubarray.findMaximumSubarray(sequence);
+        List<Double> result = Subseqs.maximumSubarray(sequence);
 
         Assert.assertArrayEquals(new Double[]{3.5, 4.8, -1.6, 7.7, 2.1}, result.toArray());
     }
 
     @Test
-    public void testFindMaximumSubarray2()
+    public void testMaximumSubarray2()
     {
         sequence = Arrays.asList(-9.3, -1.2, 3.5, 4.8, -10.6, 7.7, 2.1, 0.8, 4.0);
 
-        List<Double> result = MaximumSubarray.findMaximumSubarray(sequence);
+        List<Double> result = Subseqs.maximumSubarray(sequence);
 
         Assert.assertArrayEquals(new Double[]{7.7, 2.1, 0.8, 4.0}, result.toArray());
     }
 
     @Test
-    public void testFindMaximumSubarrayWhenAllElementsAreNegative()
+    public void testMaximumSubarrayWhenAllElementsAreNegative()
     {
         sequence = Arrays.asList(-9.0, -2.4, -3.07, -1.93, -12.67);
 
-        List<Double> result = MaximumSubarray.findMaximumSubarray(sequence);
+        List<Double> result = Subseqs.maximumSubarray(sequence);
 
         Assert.assertArrayEquals(new Double[]{}, result.toArray());
     }
 
     @Test
-    public void testFindMaximumalSum1()
+    public void testMaximalSum1()
     {
         sequence = Arrays.asList(3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8);
 
-        double result = MaximumSubarray.findMaximalSum(sequence);
+        double result = Subseqs.maximalSubsum(sequence);
 
         Assert.assertEquals(16.5, result, 0.000001);
     }
 
     @Test
-    public void testFindMaximumalSum2()
+    public void testMaximalSum2()
     {
         sequence = Arrays.asList(-9.3, -1.2, 3.5, 4.8, -10.6, 7.7, 2.1, 0.8, 4.0);
 
-        double result = MaximumSubarray.findMaximalSum(sequence);
+        double result = Subseqs.maximalSubsum(sequence);
 
         Assert.assertEquals(14.6, result, 0.000001);
     }
 
     @Test
-    public void testFindMaximumalSumWhenAllElementsAreNegative()
+    public void testMaximumalSumWhenAllElementsAreNegative()
     {
         sequence = Arrays.asList(-9.0, -2.4, -3.07, -1.93, -12.67);
 
-        double result = MaximumSubarray.findMaximalSum(sequence);
+        double result = Subseqs.maximalSubsum(sequence);
 
         Assert.assertEquals(0.0, result, 0.0);
     }
