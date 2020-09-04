15d312c3376957e6431e44b1d12da75ab47a5f0b
R. Kaleta <rafkaleta@onet.pl>
2020-09-04 23:00:27 +0200

    Renaming points sorting tests

--- test/algolib/geometry/PointsSortingTest.java
+++ test/algolib/geometry/PointsSortingTest.java
@@ -8,25 +8,7 @@ import org.junit.jupiter.api.Test;
 public class PointsSortingTest
 {
     @Test
-    public void sort2DByAngle_ThenSortedAscendingByAngleInDegrees()
-    {
-        // given
-        List<Point2D> sequence =
-                Arrays.asList(Point2D.of(0.0, 0.0), Point2D.of(-2.0, -3.0), Point2D.of(-3.0, 2.0),
-                              Point2D.of(2.0, 3.0), Point2D.of(3.0, -2.0), Point2D.of(-2.0, 3.0),
-                              Point2D.of(3.0, 2.0), Point2D.of(2.0, -3.0), Point2D.of(-3.0, -2.0));
-        // when
-        PointsSorting.sort2DByAngle(sequence);
-        // then
-        Assertions.assertThat(sequence)
-                  .containsExactly(Point2D.of(0.0, 0.0), Point2D.of(3.0, 2.0), Point2D.of(2.0, 3.0),
-                                   Point2D.of(-2.0, 3.0), Point2D.of(-3.0, 2.0),
-                                   Point2D.of(-3.0, -2.0), Point2D.of(-2.0, -3.0),
-                                   Point2D.of(2.0, -3.0), Point2D.of(3.0, -2.0));
-    }
-
-    @Test
-    public void sort2DByX_ThenSortedStablyAscendingByFirstCoordinate()
+    public void sort2DByX_ThenSortedStablyAscendingByXCoordinate()
     {
         // given
         List<Point2D> sequence =
@@ -45,7 +27,7 @@ public class PointsSortingTest
     }
 
     @Test
-    public void sort2DByY_ThenSortedStablyAscendingBySecondCoordinate()
+    public void sort2DByY_ThenSortedStablyAscendingByYCoordinate()
     {
         // given
         List<Point2D> sequence =
@@ -64,7 +46,7 @@ public class PointsSortingTest
     }
 
     @Test
-    public void sort3DByX_ThenSortedStablyAscendingByFirstCoordinate()
+    public void sort3DByX_ThenSortedStablyAscendingByXCoordinate()
     {
         // given
         List<Point3D> sequence =
@@ -83,7 +65,7 @@ public class PointsSortingTest
     }
 
     @Test
-    public void sort3DByY_ThenSortedStablyAscendingBySecondCoordinate()
+    public void sort3DByY_ThenSortedStablyAscendingByYCoordinate()
     {
         // given
         List<Point3D> sequence =
@@ -102,7 +84,7 @@ public class PointsSortingTest
     }
 
     @Test
-    public void sort3DByZ_ThenSortedStablyAscendingByThirdCoordinate()
+    public void sort3DByZ_ThenSortedStablyAscendingByZCoordinate()
     {
         // given
         List<Point3D> sequence =
@@ -118,5 +100,23 @@ public class PointsSortingTest
                                    Point3D.of(-2.0, -3.0, -5.0), Point3D.of(0.0, 0.0, 0.0),
                                    Point3D.of(-2.0, -3.0, 5.0), Point3D.of(3.0, 2.0, 5.0),
                                    Point3D.of(-3.0, 2.0, 5.0));
+    }
+
+    @Test
+    public void sort2DByAngle_ThenSortedAscendingByAngleInDegrees()
+    {
+        // given
+        List<Point2D> sequence =
+                Arrays.asList(Point2D.of(0.0, 0.0), Point2D.of(-2.0, -3.0), Point2D.of(-3.0, 2.0),
+                              Point2D.of(2.0, 3.0), Point2D.of(3.0, -2.0), Point2D.of(-2.0, 3.0),
+                              Point2D.of(3.0, 2.0), Point2D.of(2.0, -3.0), Point2D.of(-3.0, -2.0));
+        // when
+        PointsSorting.sort2DByAngle(sequence);
+        // then
+        Assertions.assertThat(sequence)
+                  .containsExactly(Point2D.of(0.0, 0.0), Point2D.of(3.0, 2.0), Point2D.of(2.0, 3.0),
+                                   Point2D.of(-2.0, 3.0), Point2D.of(-3.0, 2.0),
+                                   Point2D.of(-3.0, -2.0), Point2D.of(-2.0, -3.0),
+                                   Point2D.of(2.0, -3.0), Point2D.of(3.0, -2.0));
     }
 }
