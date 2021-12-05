a3f12db5465d0e76d1caab234c4f9920b50ae0d8
R. Kaleta <rafkaleta@onet.pl>
2021-12-05 22:01:31 +0100

    Add tests to maximum subarray

--- test/algolib/sequences/MaximumSubarrayTest.java
+++ test/algolib/sequences/MaximumSubarrayTest.java
@@ -9,13 +9,23 @@ import org.junit.jupiter.api.Test;
 public class MaximumSubarrayTest
 {
     // region findMaximumSubarray
+    @Test
+    public void findMaximumSubarray_WhenAllElementsArePositive_ThenWholeSequence()
+    {
+        // given
+        List<Double> sequence = List.of(9.0, 2.4, 3.07, 1.93, 12.67);
+        // when
+        List<Double> result = MaximumSubarray.findMaximumSubarray(sequence);
+        // then
+        Assertions.assertThat(result).containsExactlyElementsOf(sequence);
+    }
 
     @Test
     public void findMaximumSubarray_WhenNegativeIsLessThanSubsum_ThenIncludeNegative()
     {
         // when
-        List<Double> result = MaximumSubarray.findMaximumSubarray(
-                List.of(3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8));
+        List<Double> result =
+                MaximumSubarray.findMaximumSubarray(List.of(3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8));
         // then
         Assertions.assertThat(result).containsExactly(3.5, 4.8, -1.6, 7.7, 2.1);
     }
@@ -34,8 +44,8 @@ public class MaximumSubarrayTest
     public void findMaximumSubarray_WhenAllElementsAreNegative_ThenEmpty()
     {
         // when
-        List<Double> result = MaximumSubarray.findMaximumSubarray(
-                List.of(-9.0, -2.4, -3.07, -1.93, -12.67));
+        List<Double> result =
+                MaximumSubarray.findMaximumSubarray(List.of(-9.0, -2.4, -3.07, -1.93, -12.67));
         // then
         Assertions.assertThat(result).isEmpty();
     }
@@ -44,11 +54,20 @@ public class MaximumSubarrayTest
     // region countMaximalSubsum
 
     @Test
+    public void countMaximalSubsum_WhenAllElementsArePositive_ThenSumOfAll()
+    {
+        // when
+        double result = MaximumSubarray.countMaximalSubsum(List.of(9.0, 2.4, 3.07, 1.93, 12.67));
+        // then
+        Assertions.assertThat(result).isCloseTo(29.07, Offset.offset(0.000001));
+    }
+
+    @Test
     public void countMaximalSubsum_WhenNegativeIsLessThanSubsum_ThenIncludeNegative()
     {
         // when
-        double result = MaximumSubarray.countMaximalSubsum(
-                List.of(3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8));
+        double result =
+                MaximumSubarray.countMaximalSubsum(List.of(3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8));
         // then
         Assertions.assertThat(result).isCloseTo(16.5, Offset.offset(0.000001));
     }
@@ -67,8 +86,8 @@ public class MaximumSubarrayTest
     public void countMaximalSubsum_WhenAllElementsAreNegative_ThenZero()
     {
         // when
-        double result = MaximumSubarray.countMaximalSubsum(
-                List.of(-9.0, -2.4, -3.07, -1.93, -12.67));
+        double result =
+                MaximumSubarray.countMaximalSubsum(List.of(-9.0, -2.4, -3.07, -1.93, -12.67));
         // then
         Assertions.assertThat(result).isEqualTo(0.0);
     }
