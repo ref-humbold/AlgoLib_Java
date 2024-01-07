1b81e1661807679e585f05f3cd786b185b7035fb
R. Kaleta <rafkaleta@onet.pl>
2024-01-07 14:15:18 +0100

    Leftist heap

---
+++ src/main/java/algolib/structures/heaps/LeftistHeap.java
@@ -0,0 +1,185 @@
+package algolib.structures.heaps;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+/** Structure of leftist heap. */
+public class LeftistHeap<E extends Comparable<E>>
+        extends AbstractHeap<E>
+{
+    private HeapNode heap = null;
+    private int size_ = 0;
+
+    private final Comparator<E> comparator_ = Comparator.naturalOrder();
+
+    public LeftistHeap()
+    {
+        super();
+    }
+
+    public LeftistHeap(Collection<? extends E> collection)
+    {
+        super();
+        addAll(collection);
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return heap == null;
+    }
+
+    @Override
+    public Comparator<? super E> comparator()
+    {
+        return comparator_;
+    }
+
+    @Override
+    public int size()
+    {
+        return size_;
+    }
+
+    @Override
+    public void clear()
+    {
+        heap = null;
+        size_ = 0;
+    }
+
+    @Override
+    public Iterator<E> iterator()
+    {
+        return new HeapIterator();
+    }
+
+    @Override
+    public E peek()
+    {
+        return heap == null ? null : heap.element;
+    }
+
+    @Override
+    public boolean offer(E element)
+    {
+        HeapNode newNode = new HeapNode(element);
+
+        heap = heap == null ? newNode : heap.merge(newNode);
+        ++size_;
+        return true;
+    }
+
+    @Override
+    public E poll()
+    {
+        if(heap == null)
+            return null;
+
+        E value = heap.element;
+
+        heap = heap.pop();
+        --size_;
+        return value;
+    }
+
+    /**
+     * Merges given heap to this heap.
+     * @param other the heap
+     */
+    public void merge(LeftistHeap<E> other)
+    {
+        heap = heap == null ? other.heap : heap.merge(other.heap);
+        size_ += other.size_;
+    }
+
+    private final class HeapNode
+    {
+        private final int rank;
+        private final E element;
+        private final HeapNode left;
+        private final HeapNode right;
+
+        private HeapNode(E element, HeapNode node1, HeapNode node2)
+        {
+            int rank1 = node1 == null ? 0 : node1.rank;
+            int rank2 = node2 == null ? 0 : node2.rank;
+
+            this.element = element;
+
+            if(rank1 < rank2)
+            {
+                rank = rank1 + 1;
+                left = node2;
+                right = node1;
+            }
+            else
+            {
+                rank = rank2 + 1;
+                left = node1;
+                right = node2;
+            }
+        }
+
+        private HeapNode(E element)
+        {
+            this(element, null, null);
+        }
+
+        HeapNode pop()
+        {
+            return left == null ? right : left.merge(right);
+        }
+
+        HeapNode merge(HeapNode node)
+        {
+            return node == null
+                   ? this
+                   : comparator_.compare(element, node.element) < 0
+                     ? new HeapNode(element, left, right == null
+                                                   ? node
+                                                   : right.merge(node))
+                     : new HeapNode(node.element, node.left,
+                                    node.right == null ? this : node.right.merge(this));
+        }
+    }
+
+    private class HeapIterator
+            implements Iterator<E>
+    {
+        private final Stack<HeapNode> nodes = new Stack<>();
+
+        HeapIterator()
+        {
+            if(heap != null)
+                nodes.add(heap);
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return !nodes.isEmpty();
+        }
+
+        @Override
+        public E next()
+        {
+            if(!hasNext())
+                throw new NoSuchElementException("No more elements in iterator");
+
+            HeapNode node = nodes.pop();
+            E returnValue = node.element;
+
+            if(node.left != null)
+                nodes.add(node.left);
+
+            if(node.right != null)
+                nodes.add(node.right);
+
+            return returnValue;
+        }
+    }
+}
---
+++ src/test/java/algolib/structures/heaps/LeftistHeapTest.java
@@ -0,0 +1,393 @@
+package algolib.structures.heaps;
+
+import java.util.*;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// Tests: Structure of leftist heap.
+public class LeftistHeapTest
+{
+    private final Integer[] numbers =
+            new Integer[]{10, 6, 14, 97, 24, 37, 2, 30, 45, 18, 51, 71, 68, 26};
+    private final int minimum = Arrays.stream(numbers).min(Comparator.naturalOrder()).orElseThrow();
+    private LeftistHeap<Integer> testObject;
+
+    @BeforeEach
+    public void setUp()
+    {
+        testObject = new LeftistHeap<>(Arrays.asList(numbers));
+    }
+
+    @Test
+    public void constructor_WhenFromLeftistHeap_ThenCopied()
+    {
+        // when
+        var result = new LeftistHeap<>(testObject);
+        // then
+        Assertions.assertThat(result.size()).isEqualTo(testObject.size());
+        Assertions.assertThat(result.peek()).isEqualTo(testObject.peek());
+        Assertions.assertThat(result).containsExactlyInAnyOrderElementsOf(testObject);
+    }
+
+    @Test
+    public void isEmpty_WhenEmpty_ThenTrue()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+        // when
+        boolean result = testObject.isEmpty();
+        // then
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    public void isEmpty_WhenNotEmpty_ThenFalse()
+    {
+        // when
+        boolean result = testObject.isEmpty();
+        // then
+        Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    public void size_WhenEmpty_ThenZero()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+        // when
+        int result = testObject.size();
+        // then
+        Assertions.assertThat(result).isZero();
+    }
+
+    @Test
+    public void size_WhenNotEmpty_ThenNumberOfElements()
+    {
+        // when
+        int result = testObject.size();
+        // then
+        Assertions.assertThat(result).isEqualTo(numbers.length);
+    }
+
+    @Test
+    public void clear_WhenNotEmpty_ThenEmpty()
+    {
+        // when
+        testObject.clear();
+        // then
+        Assertions.assertThat(testObject).isEmpty();
+    }
+
+    @Test
+    public void iterator_WhenEmpty_ThenNoElements()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+        // when
+        Iterator<Integer> result = testObject.iterator();
+        // then
+        Assertions.assertThat(result.hasNext()).isFalse();
+        Assertions.assertThatCode(result::next).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    public void iterator_WhenSingleElement_ThenThisElementOnly()
+    {
+        // given
+        int element = 17;
+
+        testObject = new LeftistHeap<>(List.of(element));
+        // when
+        Iterator<Integer> result = testObject.iterator();
+        // then
+        Assertions.assertThat(result.hasNext()).isTrue();
+        Assertions.assertThat(result.next()).isEqualTo(element);
+        Assertions.assertThat(result.hasNext()).isFalse();
+    }
+
+    @Test
+    public void iterator_WhenMultipleElements_ThenAllElementsMinimumFirst()
+    {
+        // when
+        List<Integer> result = new ArrayList<>();
+
+        testObject.iterator().forEachRemaining(result::add);
+        // then
+        Assertions.assertThat(result).containsExactlyInAnyOrder(numbers);
+        Assertions.assertThat(result).contains(minimum, Index.atIndex(0));
+    }
+
+    // region add & offer
+
+    @Test
+    public void add_WhenNewElement_ThenAdded()
+    {
+        // given
+        int element = 46;
+        // when
+        testObject.add(element);
+        // then
+        Assertions.assertThat(testObject).hasSize(numbers.length + 1);
+        Assertions.assertThat(testObject.peek()).isEqualTo(minimum);
+    }
+
+    @Test
+    public void offer_WhenEmpty_ThenAdded()
+    {
+        // given
+        int element = 19;
+
+        testObject = new LeftistHeap<>();
+        // when
+        testObject.offer(element);
+        // then
+        Assertions.assertThat(testObject).hasSize(1);
+        Assertions.assertThat(testObject.peek()).isEqualTo(element);
+    }
+
+    @Test
+    public void offer_WhenNewElementIsLessThanMinimum_ThenNewMinimum()
+    {
+        // given
+        int element = minimum - 3;
+        // when
+        testObject.offer(element);
+        // then
+        Assertions.assertThat(testObject).hasSize(numbers.length + 1);
+        Assertions.assertThat(testObject.peek()).isEqualTo(element);
+    }
+
+    // endregion
+    // region peek & element
+
+    @Test
+    public void peek_WhenEmpty_ThenNull()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+        // when
+        Integer result = testObject.peek();
+        // then
+        Assertions.assertThat(result).isNull();
+    }
+
+    @Test
+    public void peek_WhenSingleElement_ThenThisElement()
+    {
+        // given
+        int element = 19;
+
+        testObject = new LeftistHeap<>(List.of(element));
+        // when
+        Integer result = testObject.peek();
+        // then
+        Assertions.assertThat(result).isEqualTo(element);
+    }
+
+    @Test
+    public void peek_WhenMultipleElements_ThenMinimalElement()
+    {
+        // when
+        Integer result = testObject.peek();
+        // then
+        Assertions.assertThat(result).isEqualTo(minimum);
+    }
+
+    @Test
+    public void element_WhenEmpty_ThenNoSuchElementException()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+        // when
+        Throwable throwable = Assertions.catchThrowable(() -> testObject.element());
+        // then
+        Assertions.assertThat(throwable).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    public void element_WhenSingleElement_ThenThisElement()
+    {
+        // given
+        int element = 19;
+
+        testObject = new LeftistHeap<>(List.of(element));
+        // when
+        Integer result = testObject.element();
+        // then
+        Assertions.assertThat(result).isEqualTo(element);
+    }
+
+    @Test
+    public void element_WhenMultipleElements_ThenMinimalElement()
+    {
+        // when
+        Integer result = testObject.element();
+        // then
+        Assertions.assertThat(result).isEqualTo(minimum);
+    }
+
+    // endregion
+    // region poll & remove
+
+    @Test
+    public void poll_WhenEmpty_ThenNull()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+        // when
+        Integer result = testObject.poll();
+        // then
+        Assertions.assertThat(result).isNull();
+    }
+
+    @Test
+    public void poll_WhenSingleElement_ThenThisElementRemoved()
+    {
+        // given
+        int element = 19;
+
+        testObject = new LeftistHeap<>(List.of(element));
+        // when
+        Integer result = testObject.poll();
+        // then
+        Assertions.assertThat(testObject).isEmpty();
+        Assertions.assertThat(result).isEqualTo(element);
+    }
+
+    @Test
+    public void poll_WhenMultipleElements_ThenMinimalElementRemoved()
+    {
+        // when
+        Integer result = testObject.poll();
+        // then
+        Assertions.assertThat(testObject).hasSize(numbers.length - 1);
+        Assertions.assertThat(result).isEqualTo(minimum);
+    }
+
+    @Test
+    public void remove_WhenEmpty_ThenNoSuchElementException()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+        // when
+        Throwable throwable = Assertions.catchThrowable(() -> testObject.remove());
+        // then
+        Assertions.assertThat(throwable).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    public void remove_WhenSingleElement_ThenThisElementRemoved()
+    {
+        // given
+        int element = 19;
+
+        testObject = new LeftistHeap<>(List.of(element));
+        // when
+        Integer result = testObject.remove();
+        // then
+        Assertions.assertThat(testObject).isEmpty();
+        Assertions.assertThat(result).isEqualTo(element);
+    }
+
+    @Test
+    public void remove_WhenMultipleElements_ThenMinimalElementRemoved()
+    {
+        // when
+        Integer result = testObject.remove();
+        // then
+        Assertions.assertThat(testObject).hasSize(numbers.length - 1);
+        Assertions.assertThat(result).isEqualTo(minimum);
+    }
+
+    @Test
+    public void remove_WhenMultipleCalls_ThenSorted()
+    {
+        // when
+        List<Integer> result = new ArrayList<>();
+
+        while(!testObject.isEmpty())
+            result.add(testObject.remove());
+        // then
+        Assertions.assertThat(result).hasSameElementsAs(Arrays.asList(numbers));
+        Assertions.assertThat(result).isSortedAccordingTo(Comparator.naturalOrder());
+    }
+
+    // endregion
+    //region merge
+
+    @Test
+    public void merge_WhenEmptyAndNotEmpty_ThenSameAsOther()
+    {
+        // given
+        testObject = new LeftistHeap<>();
+
+        var other = new LeftistHeap<>(Arrays.asList(numbers));
+        // when
+        testObject.merge(other);
+        // then
+        Assertions.assertThat(testObject.size()).isEqualTo(numbers.length);
+        Assertions.assertThat(testObject.peek()).isEqualTo(other.peek());
+    }
+
+    @Test
+    public void merge_WhenNotEmptyAndEmpty_ThenNoChanges()
+    {
+        // when
+        testObject.merge(new LeftistHeap<>());
+        // then
+        Assertions.assertThat(testObject.size()).isEqualTo(numbers.length);
+        Assertions.assertThat(testObject.peek()).isEqualTo(minimum);
+    }
+
+    @Test
+    public void merge_WhenOtherHasGreaterMinimum_ThenMinimumRemains()
+    {
+        // given
+        var other = new LeftistHeap<>(List.of(minimum + 5, minimum + 13, minimum + 20));
+        // when
+        testObject.merge(other);
+        // then
+        Assertions.assertThat(testObject.size()).isEqualTo(numbers.length + other.size());
+        Assertions.assertThat(testObject.peek()).isEqualTo(minimum);
+    }
+
+    @Test
+    public void merge_WhenOtherHasLessMinimum_ThenNewMinimum()
+    {
+        // given
+        int newMinimum = minimum - 4;
+        var other = new LeftistHeap<>(List.of(newMinimum, minimum + 5, minimum + 13, minimum + 20));
+        // when
+        testObject.merge(other);
+        // then
+        Assertions.assertThat(testObject.size()).isEqualTo(numbers.length + other.size());
+        Assertions.assertThat(testObject.peek()).isEqualTo(newMinimum);
+    }
+
+    @Test
+    public void merge_WhenMultipleMerges_ThenChangedOnlyMergingHeap()
+    {
+        // given
+        List<Integer> firstElements = List.of(10, 20);
+        List<Integer> secondElements = List.of(4, 8);
+
+        testObject = new LeftistHeap<>();
+        var first = new LeftistHeap<>(firstElements);
+        var second = new LeftistHeap<>(secondElements);
+        // when
+        testObject.merge(first);
+        testObject.merge(second);
+        // then
+        Assertions.assertThat(testObject.peek()).isEqualTo(4);
+        Assertions.assertThat(testObject)
+                  .containsExactlyInAnyOrderElementsOf(
+                          Stream.concat(firstElements.stream(), secondElements.stream()).toList());
+        Assertions.assertThat(first).containsExactlyInAnyOrderElementsOf(firstElements);
+        Assertions.assertThat(second).containsExactlyInAnyOrderElementsOf(secondElements);
+    }
+
+    // endregion
+}
