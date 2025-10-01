315a98db2a83877240464347bec765c77a00bca9
R. Kaleta <rafkaleta@onet.eu>
2025-10-01 19:25:19 +0200

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module algolib {
+    exports algolib.geometry;
+    exports algolib.geometry.dim2;
+    exports algolib.geometry.dim3;
+    exports algolib.graphs;
+    exports algolib.graphs.algorithms;
+    exports algolib.graphs.algorithms.strategy;
+    exports algolib.graphs.properties;
+    exports algolib.maths;
+    exports algolib.sequences;
+    exports algolib.structures;
+    exports algolib.structures.heaps;
+    exports algolib.text;
+    exports algolib.tuples;
+}
