e4729bfca72694ed71f10c1a0ddba621c14ab11d
R. Kaleta <rafkaleta@onet.eu>
2026-04-26 23:03:38 +0200

    Convex hull algorithms

--- src/main/java/com/github/refhumbold/algolib/geometry/dim2/ConvexHull.java
+++ src/main/java/com/github/refhumbold/algolib/geometry/dim2/ConvexHull.java
@@ -3,17 +3,36 @@ package com.github.refhumbold.algolib.geometry.dim2;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-/** Algorithm for convex hull in 2D (Graham's scan). */
+/** Algorithms for convex hull in 2D. */
 public final class ConvexHull
 {
     /**
-     * Computes convex hull of given points.
+     * Computes convex hull of given points using Andrew's monotone chain.
      * @param points the points
      * @return the points in the convex hull
      */
-    public static List<Point2D> findConvexHull(List<Point2D> points)
+    public static List<Point2D> findAndrewConvexHull(List<Point2D> points)
+    {
+        if(points.size() < 3)
+            return List.of();
+
+        List<Point2D> sorted = Geometry2D.sortByX(points);
+        List<Point2D> lowerHull = collectHull(sorted);
+        List<Point2D> upperHull = collectHull(sorted.reversed());
+
+        lowerHull.removeLast();
+        upperHull.removeLast();
+        return Stream.concat(lowerHull.stream(), upperHull.stream()).toList();
+    }
+
+    /**
+     * Computes convex hull of given points using Graham's scan.
+     * @param points the points
+     * @return the points in the convex hull
+     */
+    public static List<Point2D> findGrahamConvexHull(List<Point2D> points)
     {
         if(points.size() < 3)
             return List.of();
@@ -22,16 +41,20 @@ public final class ConvexHull
                                  .min(Comparator.comparingDouble((Point2D pt) -> pt.y)
                                                 .thenComparingDouble(pt -> pt.x))
                                  .orElseThrow();
-        Vector2D moving = Vector2D.between(Point2D.of(0, 0), minPoint);
-        List<Point2D> anglePoints = points.stream()
-                                          .map(pt -> Geometry2D.translate(pt, moving.negate()))
-                                          .collect(Collectors.toCollection(ArrayList::new));
+        Vector2D moving = Vector2D.between(minPoint, Point2D.ZERO);
+        List<Point2D> anglePoints = Geometry2D.sortByAngle(
+                points.stream().map(pt -> Geometry2D.translate(pt, moving)).toList());
 
-        Geometry2D.sortByAngle(anglePoints);
+        List<Point2D> hull = collectHull(anglePoints);
 
+        return hull.stream().map(pt -> Geometry2D.translate(pt, moving.negate())).toList();
+    }
+
+    private static List<Point2D> collectHull(List<Point2D> points)
+    {
         List<Point2D> hull = new ArrayList<>();
 
-        for(Point2D pt : anglePoints)
+        for(Point2D pt : points)
         {
             while(hull.size() > 1
                     && crossProduct(hull.get(hull.size() - 2), hull.getLast(), pt) >= 0)
@@ -39,8 +62,7 @@ public final class ConvexHull
 
             hull.add(pt);
         }
-
-        return hull.stream().map(pt -> Geometry2D.translate(pt, moving)).toList();
+        return hull;
     }
 
     private static double crossProduct(Point2D pt1, Point2D pt2, Point2D pt3)
--- src/test/java/com/github/refhumbold/algolib/geometry/dim2/ConvexHullTest.java
+++ src/test/java/com/github/refhumbold/algolib/geometry/dim2/ConvexHullTest.java
@@ -4,49 +4,121 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-// Tests: Algorithm for convex hull in 2D (Graham's scan).
+// Tests: Algorithms for convex hull in 2D.
 public class ConvexHullTest
 {
+    // region findAndrewConvexHull
+
     @Test
-    public void findConvexHull_WhenOnePoint_ThenEmpty()
+    public void findAndrewConvexHull_WhenOnePoint_ThenEmpty()
     {
         // when
-        List<Point2D> result = ConvexHull.findConvexHull(List.of(Point2D.of(3.0, 2.0)));
+        List<Point2D> result = ConvexHull.findAndrewConvexHull(List.of(Point2D.of(3.0, 2.0)));
 
         // then
         Assertions.assertThat(result).isEmpty();
     }
 
     @Test
-    public void findConvexHull_WhenTwoPoints_ThenEmpty()
+    public void findAndrewConvexHull_WhenTwoPoints_ThenEmpty()
     {
         // when
-        List<Point2D> result =
-                ConvexHull.findConvexHull(List.of(Point2D.of(2.0, 3.0), Point2D.of(3.0, 2.0)));
+        List<Point2D> result = ConvexHull.findAndrewConvexHull(
+                List.of(Point2D.of(2.0, 3.0), Point2D.of(3.0, 2.0)));
 
         // then
         Assertions.assertThat(result).isEmpty();
     }
 
     @Test
-    public void findConvexHull_WhenThreePoints_ThenThesePointsInHull()
+    public void findAndrewConvexHull_WhenThreePoints_ThenThesePointsInHull()
     {
         // given
         List<Point2D> points =
                 List.of(Point2D.of(1.0, -1.0), Point2D.of(5.0, 1.0), Point2D.of(3.0, 4.0));
 
         // when
-        List<Point2D> result = ConvexHull.findConvexHull(points);
+        List<Point2D> result = ConvexHull.findAndrewConvexHull(points);
 
         // then
         Assertions.assertThat(result).isEqualTo(points);
     }
 
     @Test
-    public void findConvexHull_ThenPointsInHull()
+    public void findAndrewConvexHull_ThenPointsInHull()
     {
         // when
-        List<Point2D> result = ConvexHull.findConvexHull(
+        List<Point2D> result = ConvexHull.findAndrewConvexHull(
+                List.of(Point2D.of(1, -3), Point2D.of(-4, 6), Point2D.of(-5, -7),
+                        Point2D.of(-8, -7), Point2D.of(-3, -4), Point2D.of(5, 9),
+                        Point2D.of(-1, -8), Point2D.of(-5, 10), Point2D.of(8, 0), Point2D.of(3, -6),
+                        Point2D.of(-2, 1), Point2D.of(-2, 8), Point2D.of(10, 2), Point2D.of(6, 3),
+                        Point2D.of(-7, 7), Point2D.of(6, -4)));
+
+        // then
+        Assertions.assertThat(result)
+                  .containsExactly(Point2D.of(-8, -7), Point2D.of(-1, -8), Point2D.of(3, -6),
+                          Point2D.of(6, -4), Point2D.of(10, 2), Point2D.of(5, 9),
+                          Point2D.of(-5, 10), Point2D.of(-7, 7));
+    }
+
+    @Test
+    public void findAndrewConvexHull_WhenMultiplePointsAreCollinear_ThenInnerPointsOmitted()
+    {
+        // when
+        List<Point2D> result = ConvexHull.findAndrewConvexHull(
+                List.of(Point2D.of(-1, -3), Point2D.of(-3, -3), Point2D.of(-3, -1),
+                        Point2D.of(2, -3), Point2D.of(-3, 5), Point2D.of(0, -3), Point2D.of(7, -3),
+                        Point2D.of(-3, -2)));
+
+        // then
+        Assertions.assertThat(result)
+                  .containsExactly(Point2D.of(-3, -3), Point2D.of(7, -3), Point2D.of(-3, 5));
+    }
+
+    // endregion
+    // region findGrahamConvexHull
+
+    @Test
+    public void findGrahamConvexHull_WhenOnePoint_ThenEmpty()
+    {
+        // when
+        List<Point2D> result = ConvexHull.findGrahamConvexHull(List.of(Point2D.of(3.0, 2.0)));
+
+        // then
+        Assertions.assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void findGrahamConvexHull_WhenTwoPoints_ThenEmpty()
+    {
+        // when
+        List<Point2D> result = ConvexHull.findGrahamConvexHull(
+                List.of(Point2D.of(2.0, 3.0), Point2D.of(3.0, 2.0)));
+
+        // then
+        Assertions.assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void findGrahamConvexHull_WhenThreePoints_ThenThesePointsInHull()
+    {
+        // given
+        List<Point2D> points =
+                List.of(Point2D.of(1.0, -1.0), Point2D.of(5.0, 1.0), Point2D.of(3.0, 4.0));
+
+        // when
+        List<Point2D> result = ConvexHull.findGrahamConvexHull(points);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(points);
+    }
+
+    @Test
+    public void findGrahamConvexHull_ThenPointsInHull()
+    {
+        // when
+        List<Point2D> result = ConvexHull.findGrahamConvexHull(
                 List.of(Point2D.of(1, -3), Point2D.of(-4, 6), Point2D.of(-5, -7),
                         Point2D.of(-8, -7), Point2D.of(-3, -4), Point2D.of(5, 9),
                         Point2D.of(-1, -8), Point2D.of(-5, 10), Point2D.of(8, 0), Point2D.of(3, -6),
@@ -61,10 +133,10 @@ public class ConvexHullTest
     }
 
     @Test
-    public void findConvexHull_WhenMultiplePointsAreCollinear_ThenInnerPointsOmitted()
+    public void findGrahamConvexHull_WhenMultiplePointsAreCollinear_ThenInnerPointsOmitted()
     {
         // when
-        List<Point2D> result = ConvexHull.findConvexHull(
+        List<Point2D> result = ConvexHull.findGrahamConvexHull(
                 List.of(Point2D.of(-1, -3), Point2D.of(-3, -3), Point2D.of(-3, -1),
                         Point2D.of(2, -3), Point2D.of(-3, 5), Point2D.of(0, -3), Point2D.of(7, -3),
                         Point2D.of(-3, -2)));
@@ -73,4 +145,6 @@ public class ConvexHullTest
         Assertions.assertThat(result)
                   .containsExactly(Point2D.of(-3, -3), Point2D.of(7, -3), Point2D.of(-3, 5));
     }
+
+    // endregion
 }
